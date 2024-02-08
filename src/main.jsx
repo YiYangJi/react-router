d546967bd7ab3b8f1374d26ee386f7c57d98ec1f
YiYangJi <alumno.621144@ies-azarquiel.es>
2024-02-08 13:41:15 +0100

    error-page component implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
