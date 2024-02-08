378040fa101d3806ea5f35ed25d311b8d8ec1066
YiYangJi <alumno.621144@ies-azarquiel.es>
2024-02-08 13:56:57 +0100

    loader configured

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import Root, { loader as rootLoader } from "./routes/root";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
