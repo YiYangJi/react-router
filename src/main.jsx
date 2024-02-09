b3dbee9aa4f1af69dffce3b27c8f4e82c534d7d5
YiYangJi <alumno.621144@ies-azarquiel.es>
2024-02-09 10:20:04 +0100

    EditContact implemented

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import EditContact from "./routes/edit";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
@@ -17,6 +18,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
